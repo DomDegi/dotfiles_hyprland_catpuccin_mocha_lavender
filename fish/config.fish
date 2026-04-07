1ddd9893c5eea4571cf67399879f7669a9b0d550
domdegi <domenico.degiorgio@mail.polimi.it>
2026-04-07 18:14:41 +0200

    latest update

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,9 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 fish_add_path /home/domdegi/.spicetify
 
+export PATH="$HOME/.local/bin:$PATH"
+
+# Battery Conservation Shortcuts
+abbr -a bat_limit "echo 1 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode > /dev/null"
+abbr -a bat_full  "echo 0 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode > /dev/null"
+
